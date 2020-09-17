98e3845488ec196125a65a73666a4a7bf49aa3f0
Surya <steja1122@gmail.com>
2020-09-17 10:10:32 +0530

    Initial Commit

---
+++ Amigosinpin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amigosinpin
+//
+//  Created by Surya Chowdary on 17/09/20.
+//  Copyright © 2020 WebMobRil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
